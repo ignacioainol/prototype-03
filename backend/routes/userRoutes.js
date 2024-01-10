748d4669550d52247197fc7949be0750313323d8
Ignacio Ainol <iggnaxios@gmail.com>
2024-01-09 23:15:47 -0300

    Create User profile screen

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import User from '../models/userModel.js';
 import expressAsyncHandler from 'express-async-handler';
-import { generateToken } from '../utils.js';
+import { generateToken, isAuth } from '../utils.js';
 import bcrypt from 'bcryptjs';
 const userRouter = express.Router();
 
@@ -25,6 +25,8 @@ userRouter.post(
   })
 );
 
+userRouter.put('/profile');
+
 userRouter.post(
   '/signin',
   expressAsyncHandler(async (req, res) => {
@@ -42,6 +44,34 @@ userRouter.post(
       }
     }
     res.status(401).send({ message: 'Invalid email or password' });
+  })
+);
+
+userRouter.put(
+  '/profile',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    } else {
+      res.status(404).send({
+        message: 'User not found',
+      });
+    }
   })
 );
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import { PaymentScreen } from './screens/PaymentScreen';
 import { PlaceOrderScreen } from './screens/PlaceOrderScreen';
 import { OrderScreen } from './screens/OrderScreen';
 import { OrderHistoryScreen } from './screens/OrderHistoryScreen';
+import { ProfileScreen } from './screens/ProfileScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -27,6 +28,7 @@ function App() {
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
     localStorage.removeItem('paymentMethod');
+    window.location.href = '/signin';
   };
   return (
     <BrowserRouter>
@@ -85,6 +87,7 @@ function App() {
               <Route path="/signup" element={<SignupScreen />}></Route>
               <Route path="/payment" element={<PaymentScreen />}></Route>
               <Route path="/placeorder" element={<PlaceOrderScreen />}></Route>
+              <Route path="/profile" element={<ProfileScreen />}></Route>
               <Route path="/order/:id" element={<OrderScreen />}></Route>
               <Route
                 path="/orderhistory"
